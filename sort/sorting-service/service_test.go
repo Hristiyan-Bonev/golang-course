c799bbf2e18b591923eea1483fa6fcf752ea8377
Hristiyan Bonev <hristiyan.bonev@ocado.com>
2021-06-13 00:57:47 +0300

    Add test for MoveItem; Add one test case for SelectItem

--- sort/sorting-service/service_test.go
+++ sort/sorting-service/service_test.go
@@ -22,8 +22,6 @@ func TestLoadItems(t *testing.T) {
 			in: []*gen.Item{
 				{Code: "123-456-789", Label: "foo shirt"},
 				{Code: "234-123-123", Label: "foo pants"},
-				{Code: "334-123-123", Label: "foo jacket"},
-				{Code: "123-410-213", Label: "foo hat"},
 			},
 			errorString: "",
 		},
@@ -55,9 +53,10 @@ func TestLoadItems(t *testing.T) {
 func TestSelectItem(t *testing.T) {
 
 	testCases := []struct {
-		testName     string
-		initialItems []*gen.Item
-		out          *gen.Item
+		testName      string
+		initialItems  []*gen.Item
+		out           *gen.Item
+		expectedError bool
 	}{
 		{
 			testName: "pick with single item in inventory",
@@ -66,6 +65,11 @@ func TestSelectItem(t *testing.T) {
 				{Code: "999-888", Label: "foo shirt"},
 			},
 			out: &gen.Item{Code: "123-456", Label: "foo shirt"},
+		},
+		{
+			testName:      "pick with single item in inventory",
+			initialItems:  []*gen.Item{},
+			expectedError: true,
 		},
 	}
 
@@ -76,12 +80,53 @@ func TestSelectItem(t *testing.T) {
 
 			_, err := service.SelectItem(context.Background(), &gen.SelectItemRequest{})
 
-			if err != nil {
-				t.Fatalf("unable to select item: %v", err)
+			if err != nil && !tc.expectedError {
+				t.Fatalf("unexpected error: %v", err)
 			}
 
-			// Assert one item is removed
-			assert.Equal(t, len(tc.initialItems)-1, len(service.Items))
+			if !tc.expectedError {
+				// Ensure one item is discarded upon SelectItem
+				assert.Equal(t, len(tc.initialItems)-1, len(service.Items))
+			}
 		})
 	}
+}
+
+func TestMoveItem(t *testing.T) {
+
+	service := newSortingService()
+
+	service.Items = []*gen.Item{
+		{Code: "123-456", Label: "foo shirt"},
+		{Code: "999-888", Label: "foo shirt"},
+	}
+
+	// First select and move
+	if _, err := service.SelectItem(context.Background(), &gen.SelectItemRequest{}); err != nil {
+		t.Fatalf("cannot select item: %v", err)
+	}
+	if _, err := service.MoveItem(context.Background(), &gen.MoveItemRequest{Cubby: &gen.Cubby{Id: "A1"}}); err != nil {
+		t.Fatalf("cannot move item: %v", err)
+	}
+
+	// Second select and move
+	if _, err := service.SelectItem(context.Background(), &gen.SelectItemRequest{}); err != nil {
+		t.Fatalf("cannot select item: %v", err)
+	}
+
+	if _, err := service.MoveItem(context.Background(), &gen.MoveItemRequest{Cubby: &gen.Cubby{Id: "A2"}}); err != nil {
+		t.Fatalf("cannot move item: %v", err)
+	}
+
+	// Verify that items are placed in the appropriate cubbies
+	assert.Equal(t, len(service.Cubbies["A1"]), 1)
+	assert.Equal(t, len(service.Cubbies["A2"]), 1)
+
+	// Third select and move - should fail
+	_, selectErr := service.SelectItem(context.Background(), &gen.SelectItemRequest{})
+	_, moveErr := service.MoveItem(context.Background(), &gen.MoveItemRequest{Cubby: &gen.Cubby{Id: "ZZ"}})
+
+	assert.NotEqual(t, nil, selectErr)
+	assert.NotEqual(t, nil, moveErr)
+
 }
